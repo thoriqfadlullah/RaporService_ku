d52d03bbca949e83a40bdbbe9009e495bd310cfb
M. Thoriq Fadlullah <mthoriqfadlullah@outlook.co.id>
2019-12-12 20:38:29 +0700

    Add project files.

---
+++ RaporService/IService1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RaporService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        //[OperationContract]
+        //[WebInvoke(Method = "GET",
+        //    ResponseFormat = WebMessageFormat.Json,
+        //    RequestFormat = WebMessageFormat.Json,
+        //    UriTemplate = "getdata/id={id}"
+        //    )]
+        //DataPesanan GetData(string id);
+
+        // get data admin
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            UriTemplate = "dataAdmin")]
+        List<Admin> GetAdmin();
+        
+        // get data kelas
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            UriTemplate = "dataKelas")]
+        List<Kelas> GetKelas();
+
+        // get data mapel
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            UriTemplate = "dataMapel")]
+        List<Mapel> GetMapel();
+
+        // get data rapot
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            UriTemplate = "dataRapot")]
+        List<Rapot> GetRapot();
+        
+        // get data siswa
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            UriTemplate = "dataSiswa")]
+        List<Siswa> GetSiswa();
+
+        // get data waliKelas
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            UriTemplate = "dataWaliKelas")]
+        List<WaliKelas> GetWaliKelas();
+
+        // add data admin
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            UriTemplate = "getAdmin/id={id}")]
+        Admin LoginAdmin(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            UriTemplate = "getWalikelas/id={id}")]
+        WaliKelas GetWalikelas(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            UriTemplate = "getKelasSiswa/id={id}")]
+        List<Siswa> GetKelasSiswa(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            UriTemplate = "getNilaiRapot/id={id}")]
+        List<Rapot> GetNilaiRapot(string id);
+
+        // add data kelas
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            UriTemplate = "adddataKelas")]
+        string AddDataKelas(Kelas dp);
+
+        // add data mapel
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            UriTemplate = "adddataMapel")]
+        string AddDataMapel(Mapel dp);
+
+        // add data rapot
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            UriTemplate = "adddataRapot")]
+        string AddDataRapot(Rapot dp);
+
+        // add data siswa
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            UriTemplate = "adddataSiswa")]
+        string AddDataSiswa(Siswa dp);
+
+        // add data walikelas
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            UriTemplate = "adddataWaliKelas")]
+        string AddDataWaliKelas(WaliKelas dp);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            UriTemplate = "updatedataWaliKelas")]
+        string UpdateDataWaliKelas(WaliKelas dp);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            UriTemplate = "updatedatasiswa")]
+        string updatedatasiswa(Siswa dp);
+    }
+
+    [DataContract]
+    public class Admin
+    {
+        [DataMember]
+        public string id_admin { get; set; }
+        [DataMember]
+        public string uname_admin { get; set; }
+        [DataMember]
+        public string passw_admin { get; set; }
+    }
+
+    [DataContract]
+    public class Kelas
+    {
+        [DataMember]
+        public string id_kelas { get; set; }
+        [DataMember]
+        public string nama_kelas { get; set; }
+    }
+
+    [DataContract]
+    public class Mapel
+    {
+        [DataMember]
+        public string id_mapel { get; set; }
+        [DataMember]
+        public string nama_mapel { get; set; }
+        [DataMember]
+        public string nama_jurusan { get; set; }
+    }
+
+    [DataContract]
+    public class Rapot
+    {
+        [DataMember]
+        public string nilai { get; set; }
+        [DataMember]
+        public string semester { get; set; }
+        [DataMember]
+        public string id_kelas { get; set; }
+        [DataMember]
+        public string id_mapel {get; set;}
+        [DataMember]
+        public string id_siswa { get; set; }
+        [DataMember]
+        public string id_rapot { get; set; }
+    }
+
+    [DataContract]
+    public class Siswa
+    {
+        [DataMember]
+        public string tempat_lahir { get; set; }
+        [DataMember]
+        public string tgl_lahir { get; set; }
+        [DataMember]
+        public string id_kelas { get; set; }
+        [DataMember]
+        public string nama_siswa { get; set; }
+        [DataMember]
+        public string id_siswa { get; set; }
+        [DataMember]
+        public string id_walikelas { get; set; }
+        [DataMember]
+        public string alamat { get; set; }
+        [DataMember]
+        public string nama_ibu { get; set; }
+        [DataMember]
+        public string nomor_ortu { get; set; }
+        [DataMember]
+        public string jenis_kelamin { get; set; }
+        [DataMember]
+        public string status_kawin { get; set; }
+        [DataMember]
+        public string nama_ayah { get; set; }
+        [DataMember]
+        public string nama_agama { get; set; }
+    }
+
+    [DataContract]
+    public class WaliKelas
+    {
+        [DataMember]
+        public string id_walikelas { get; set; }
+        [DataMember]
+        public string nama_walikelas { get; set; }
+        [DataMember]
+        public string uname_walikelas { get; set; }
+        [DataMember]
+        public string passw_walikelas { get; set; }
+        [DataMember]
+        public string id_kelas { get; set; }
+    }
+}
---
+++ RaporService/Service1.svc.cs
@@ -0,0 +1,438 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Configuration;
+
+namespace RaporService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        SqlConnection conn;
+        SqlCommand comm;
+        SqlConnectionStringBuilder connStringBuilder;
+        public Service1()
+        {
+            //DBConnect();
+            string connstring = ConfigurationManager.ConnectionStrings["dbConn"].ConnectionString;
+            conn = new SqlConnection(connstring);
+            comm = conn.CreateCommand();
+        }
+
+        void DBConnect()
+        {
+            connStringBuilder = new SqlConnectionStringBuilder();
+            connStringBuilder.DataSource = "DESKTOP-DK319Q3";
+            connStringBuilder.InitialCatalog = "raporku";
+            connStringBuilder.Encrypt = true;
+            connStringBuilder.TrustServerCertificate = true;
+            connStringBuilder.ConnectTimeout = 30;
+            connStringBuilder.AsynchronousProcessing = true;
+            connStringBuilder.MultipleActiveResultSets = true;
+            connStringBuilder.IntegratedSecurity = true;
+
+            conn = new SqlConnection(connStringBuilder.ToString());
+            comm = conn.CreateCommand();
+        }
+
+        //public DataPesanan GetData(string id)
+        //{
+        //    DataPesanan dp = new DataPesanan();
+        //    SqlCommand sqlCommand = new SqlCommand("select * from pesanan where id = " + id, conn);
+        //    conn.Open();
+        //    SqlDataReader reader = sqlCommand.ExecuteReader();
+
+        //    while (reader.Read())
+        //    {
+        //        dp.id = reader["id"].ToString();
+        //        dp.pesanan = reader["pesanan"].ToString();
+        //        dp.harga = reader["harga"].ToString();
+        //    }
+        //    conn.Close();
+        //    return dp;
+        //}
+
+        public List<Admin> GetAdmin()
+        {
+            //string query = String.Format("Update pesanan set pesanan")
+            List<Admin> dp = new List<Admin>();
+            SqlCommand cmd = new SqlCommand("select * from admin", conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Admin pesan = new Admin();
+                pesan.id_admin = reader["ID_Admin"].ToString();
+                pesan.uname_admin = reader["Username_adm"].ToString();
+                pesan.passw_admin = reader["Password_adm"].ToString();
+                dp.Add(pesan);
+            }
+            conn.Close();
+            return dp;
+        }
+
+        public List<Kelas> GetKelas()
+        {
+            //string query = String.Format("Update pesanan set pesanan")
+            List<Kelas> dp = new List<Kelas>();
+            SqlCommand cmd = new SqlCommand("select * from kelas", conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Kelas pesan = new Kelas();
+                pesan.id_kelas = reader["ID_Kelas"].ToString();
+                pesan.nama_kelas = reader["Nama_Kelas"].ToString();
+                dp.Add(pesan);
+            }
+            conn.Close();
+            return dp;
+        }
+
+        public List<Mapel> GetMapel()
+        {
+            //string query = String.Format("Update pesanan set pesanan")
+            List<Mapel> dp = new List<Mapel>();
+            SqlCommand cmd = new SqlCommand("select * from mapel", conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Mapel pesan = new Mapel();
+                pesan.id_mapel = reader["ID_Mapel"].ToString();
+                pesan.nama_mapel = reader["Nama_Mapel"].ToString();
+                pesan.nama_jurusan = reader["Nama_Jurusan"].ToString();
+                dp.Add(pesan);
+            }
+            conn.Close();
+            return dp;
+        }
+
+        public List<Rapot> GetRapot()
+        {
+            //string query = String.Format("Update pesanan set pesanan")
+            List<Rapot> dp = new List<Rapot>();
+            SqlCommand cmd = new SqlCommand("select * from rapot", conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Rapot pesan = new Rapot();
+                pesan.nilai = reader["Nilai"].ToString();
+                pesan.semester = reader["Semester"].ToString();
+                pesan.id_kelas = reader["ID_kelas"].ToString();
+                pesan.id_mapel = reader["ID_mapel"].ToString();
+                pesan.id_siswa = reader["ID_siswa"].ToString();
+                pesan.id_rapot = reader["ID_rapot"].ToString();
+                dp.Add(pesan);
+            }
+            conn.Close();
+            return dp;
+        }
+
+        public List<Siswa> GetSiswa()
+        {
+            //string query = String.Format("Update pesanan set pesanan")
+            List<Siswa> dp = new List<Siswa>();
+            SqlCommand cmd = new SqlCommand("select * from siswa", conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Siswa pesan = new Siswa();
+                pesan.tempat_lahir = reader["Tempat_lahir"].ToString();
+                pesan.tgl_lahir = reader["Tanggal_lahir"].ToString();
+                pesan.id_kelas = reader["ID_kelas"].ToString();
+                pesan.nama_siswa = reader["Nama_siswa"].ToString();
+                pesan.id_siswa = reader["ID_siswa"].ToString();
+                pesan.id_walikelas = reader["ID_wali_kelas"].ToString();
+                pesan.alamat = reader["Alamat"].ToString();
+                pesan.nama_ibu = reader["Nama_Ibu"].ToString();
+                pesan.nomor_ortu = reader["Nomor_ortu"].ToString();
+                pesan.jenis_kelamin = reader["Jenis_kelamin"].ToString();
+                pesan.status_kawin = reader["status_kawin"].ToString();
+                pesan.nama_ayah = reader["Nama_ayah"].ToString();
+                pesan.nama_agama = reader["Nama_agama"].ToString();
+                dp.Add(pesan);
+            }
+            conn.Close();
+            return dp;
+        }
+
+        public List<WaliKelas> GetWaliKelas()
+        {
+            //string query = String.Format("Update pesanan set pesanan")
+            List<WaliKelas> dp = new List<WaliKelas>();
+            SqlCommand cmd = new SqlCommand("select * from walikelas", conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                WaliKelas pesan = new WaliKelas();
+                pesan.id_walikelas = reader["ID_Wali_Kelas"].ToString();
+                pesan.nama_walikelas = reader["Nama_Wali_Kelas"].ToString();
+                pesan.uname_walikelas = reader["Username_wk"].ToString();
+                pesan.passw_walikelas = reader["Password_wk"].ToString();
+                pesan.id_kelas = reader["ID_Kelas"].ToString();
+                dp.Add(pesan);
+            }
+            conn.Close();
+            return dp;
+        }
+
+        public Admin LoginAdmin(string id)
+        {
+            Admin ad = new Admin();
+            SqlCommand cmd = new SqlCommand("select * from admin where Username_adm = " + "'"+ id + "'", conn);
+            //    SqlCommand sqlCommand = new SqlCommand("select * from pesanan where id = " + id, conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                ad.id_admin = reader["ID_Admin"].ToString();
+                ad.uname_admin = reader["Username_adm"].ToString();
+                ad.passw_admin = reader["Password_adm"].ToString();
+            }
+            conn.Close();
+            return ad;
+
+
+            //SqlCommand cmd = new SqlCommand("insert into admin(ID_Admin,Username_adm,Password_adm) values(" +
+            //    "'" + dp.id_admin + "'," +
+            //    "'" + dp.uname_admin + "'," +
+            //    "'" + dp.passw_admin + "')", conn);
+            //try
+            //{
+            //    conn.Open();
+            //    cmd.ExecuteNonQuery();
+            //    conn.Close();
+            //    return "Data Berhasil Di Simpan";
+            //}
+            //catch (Exception ex)
+            //{
+            //    return "Data Gagal Di Simpan : " + ex;
+            //}
+        }
+
+        public WaliKelas GetWalikelas(string id)
+        {
+            WaliKelas ad = new WaliKelas();
+            SqlCommand cmd = new SqlCommand("select * from walikelas where Nama_Wali_Kelas = " + "'" + id + "'", conn);
+            //    SqlCommand sqlCommand = new SqlCommand("select * from pesanan where id = " + id, conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                ad.id_walikelas = reader["ID_Wali_Kelas"].ToString();
+                ad.nama_walikelas = reader["Nama_Wali_Kelas"].ToString();
+                ad.uname_walikelas = reader["Username_wk"].ToString();
+                ad.passw_walikelas = reader["Password_wk"].ToString();
+                ad.id_kelas = reader["ID_Kelas"].ToString();
+            }
+            conn.Close();
+            return ad;
+        }
+
+        public List<Siswa> GetKelasSiswa(string id)
+        {
+            List<Siswa> dp = new List<Siswa>();
+            SqlCommand cmd = new SqlCommand("select * from siswa where ID_kelas = " + "'" + id + "'", conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Siswa pesan = new Siswa();
+                pesan.tempat_lahir = reader["Tempat_lahir"].ToString();
+                pesan.tgl_lahir = reader["Tanggal_lahir"].ToString();
+                pesan.id_kelas = reader["ID_kelas"].ToString();
+                pesan.nama_siswa = reader["Nama_siswa"].ToString();
+                pesan.id_siswa = reader["ID_siswa"].ToString();
+                pesan.id_walikelas = reader["ID_wali_kelas"].ToString();
+                pesan.alamat = reader["Alamat"].ToString();
+                pesan.nama_ibu = reader["Nama_Ibu"].ToString();
+                pesan.nomor_ortu = reader["Nomor_ortu"].ToString();
+                pesan.jenis_kelamin = reader["Jenis_kelamin"].ToString();
+                pesan.status_kawin = reader["status_kawin"].ToString();
+                pesan.nama_ayah = reader["Nama_ayah"].ToString();
+                pesan.nama_agama = reader["Nama_agama"].ToString();
+                dp.Add(pesan);
+            }
+            conn.Close();
+            return dp;
+        }
+
+        public List<Rapot> GetNilaiRapot(string id)
+        {
+            List<Rapot> dp = new List<Rapot>();
+            SqlCommand cmd = new SqlCommand("select * from rapot where ID_siswa = " + "'" + id + "'", conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Rapot pesan = new Rapot();
+                pesan.nilai = reader["Nilai"].ToString();
+                pesan.semester = reader["Semester"].ToString();
+                pesan.id_kelas = reader["ID_kelas"].ToString();
+                pesan.id_mapel = reader["ID_mapel"].ToString();
+                pesan.id_siswa = reader["ID_siswa"].ToString();
+                pesan.id_rapot = reader["ID_rapot"].ToString();
+                dp.Add(pesan);
+            }
+            conn.Close();
+            return dp;
+        }
+
+        public string AddDataKelas(Kelas dp)
+        {
+            SqlCommand cmd = new SqlCommand("insert into kelas(ID_Kelas,Nama_Kelas) values(" +
+                "'" + dp.id_kelas + "'," +
+                "'" + dp.nama_kelas + "')", conn);
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                return "Data Berhasil Di Simpan";
+            }
+            catch (Exception ex)
+            {
+                return "Data Gagal Di Simpan : " + ex;
+            }
+        }
+
+        public string AddDataMapel(Mapel dp)
+        {
+            SqlCommand cmd = new SqlCommand("insert into mapel(ID_Mapel,Nama_Mapel,Nama_Jurusan) values(" +
+                "'" + dp.id_mapel + "'," +
+                "'" + dp.nama_mapel + "'," +
+                "'" + dp.nama_jurusan + "')", conn);
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                return "Data Berhasil Di Simpan";
+            }
+            catch (Exception ex)
+            {
+                return "Data Gagal Di Simpan : " + ex;
+            }
+        }
+
+        public string AddDataRapot(Rapot dp)
+        {
+            SqlCommand cmd = new SqlCommand("insert into rapot(Nilai,Semester,ID_kelas,ID_mapel,ID_siswa,ID_rapot) values(" +
+                "'" + dp.nilai + "'," +
+                "'" + dp.semester + "'," +
+                "'" + dp.id_kelas + "'," +
+                "'" + dp.id_mapel + "'," +
+                "'" + dp.id_siswa + "'," +
+                "'" + dp.id_rapot + "')", conn);
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                return "Data Berhasil Di Simpan";
+            }
+            catch (Exception ex)
+            {
+                return "Data Gagal Di Simpan : " + ex;
+            }
+        }
+
+        public string AddDataSiswa(Siswa dp)
+        {
+            SqlCommand cmd = new SqlCommand("insert into siswa(Tempat_lahir,Tanggal_lahir,ID_kelas,Nama_siswa,ID_wali_kelas,Alamat,Nama_Ibu,Nomor_ortu,Jenis_kelamin,status_kawin,Nama_ayah,Nama_agama) values(" +
+                "'" + dp.tempat_lahir + "'," +
+                "'" + dp.tgl_lahir + "'," +
+                "'" + dp.id_kelas + "'," +
+                "'" + dp.nama_siswa + "'," +
+                "'" + dp.id_walikelas + "'," +
+                "'" + dp.alamat + "'," +
+                "'" + dp.nama_ibu + "'," +
+                "'" + dp.nomor_ortu + "'," +
+                "'" + dp.jenis_kelamin + "'," +
+                "'" + dp.status_kawin + "'," +
+                "'" + dp.nama_ayah + "'," +
+                "'" + dp.nama_agama + "')", conn);
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                return "Data Berhasil Di Simpan";
+            }
+            catch (Exception ex)
+            {
+                return "Data Gagal Di Simpan : " + ex;
+            }
+        }
+
+        public string AddDataWaliKelas(WaliKelas dp)
+        {
+            SqlCommand cmd = new SqlCommand("insert into walikelas(Nama_Wali_Kelas,Username_wk,Password_wk,ID_Kelas) values(" +
+                "'" + dp.nama_walikelas + "'," +
+                "'" + dp.uname_walikelas + "'," +
+                "'" + dp.passw_walikelas + "'," +
+                "'" + dp.id_kelas + "')", conn);
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                return "Data Berhasil Di Simpan";
+            }
+            catch (Exception ex)
+            {
+                return "Data Gagal Di Simpan : " + ex;
+            }
+        }
+
+        public string UpdateDataWaliKelas(WaliKelas dp)
+        {
+            SqlCommand cmd = new SqlCommand("update walikelas set Nama_Wali_Kelas='" + dp.nama_walikelas + "'" +
+                ", Username_wk='" + dp.uname_walikelas + "', Password_wk='" + dp.passw_walikelas + "'" +
+                ", ID_Kelas='" + dp.id_kelas + "' where ID_Wali_Kelas = " + "'" + dp.id_walikelas +"'", conn);
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                return "Data Berhasil Di Update";
+            }
+            catch (Exception ex)
+            {
+                return "Data Gagal Di Update : " + ex;
+            }
+        }
+
+        public string updatedatasiswa(Siswa dp)
+        {
+            SqlCommand cmd = new SqlCommand("update siswa set Tempat_lahir='" + dp.tempat_lahir + "'" +
+                ", Tanggal_lahir='" + dp.tgl_lahir + "', ID_kelas='" + dp.id_kelas + "', Nama_siswa='" + dp.nama_siswa + "'" +
+                ", ID_wali_kelas='" + dp.id_walikelas + "', Alamat='" + dp.alamat + "'" + ", Nama_Ibu='" + dp.nama_ibu + "'" +
+                ", Nomor_ortu='" + dp.nomor_ortu + "', Jenis_kelamin='" + dp.jenis_kelamin + "'" + ", status_kawin='" + dp.status_kawin + "'" +
+                ", Nama_ayah='" + dp.nama_ayah + "', Nama_agama='" + dp.nama_agama + "'" + " where ID_siswa = " + "'" + dp.id_siswa + "'", conn);
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                return "Data Berhasil Di Update";
+            }
+            catch (Exception ex)
+            {
+                return "Data Gagal Di Update : " + ex;
+            }
+        }
+    }
+}
